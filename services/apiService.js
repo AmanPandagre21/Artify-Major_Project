15adc1320a92874ec5ecfd1c4e7f4f813a52480a
Aman Pandagre <amanpandagre21@gmail.com>
2022-11-27 18:03:24 +0530

    change backend url

--- services/apiService.js
+++ services/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://artify-app-server.herokuapp.com/api/v1",
+  baseURL: "https://artify-backend.onrender.com/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
